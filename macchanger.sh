29c03a42c4a19e06d0cc5ae69dd97bfc7c1d4454
parrot2023 <parrot.learn2023@gmail.com>
2023-09-19 23:33:28 +0545

    Setup For Fresh Parrot Install and MacChanger

---
+++ macchanger.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#set -x
+
+#sudo macchanger -m 8c:c8:cd:**:**:** enp2s0
+#sudo macchanger -e enp2s0
+#sudo macchanger -e enp2s0
+#sudo macchanger -e enp2s0
+#
+#exit
+# n=1
+#while(( $n < 20 ))
+INTERFACES=$(ls /sys/class/net > /tmp/interfaces_list.file)
+#echo "$INTERFACES"
+while read INTERFACE
+do
+    if [ $INTERFACE != "lo" ]; then
+        echo "Shutting down Interface $INTERFACE........."
+        echo $(sudo ifconfig $INTERFACE down)
+        #original_mac=$(sudo macchanger -s $INTERFACE)
+        wait
+        
+        if [[ "$INTERFACE" =~ vm ]]; then
+            #new_mac=$(sudo macchanger -s $INTERFACE)
+            #while (( $original_mac -ne $new_mac ))
+            #do
+                echo "Changing MAC Address For Interface $INTERFACE........."
+                echo $(sudo macchanger -e $INTERFACE)
+                echo "....................................................................."
+                echo $(sudo macchanger -e $INTERFACE)
+                echo "....................................................................."
+                echo $(sudo macchanger -e $INTERFACE)
+                echo "....................................................................."
+             #   new_mac=$(sudo macchanger -s $INTERFACE)
+            #done
+        else
+            echo "Changing MAC Address of Interface $INTERFACE........."
+            echo $(sudo macchanger -a $INTERFACE)
+            echo "....................................................................."
+            echo $(sudo macchanger -a $INTERFACE)
+            echo "....................................................................."
+            echo $(sudo macchanger -a $INTERFACE)
+            echo "....................................................................."
+        fi
+
+        echo "Starting Interface $INTERFACE........."
+        sudo ifconfig $INTERFACE up
+        echo "=========================================================================="
+        echo "=========================================================================="
+        echo ""
+    fi
+done < /tmp/interfaces_list.file
---
+++ setup.sh
@@ -0,0 +1,120 @@
+#!/bin/bash
+
+#set -x #debugging mode
+
+ROOT_PASSWORD="$1"
+DESKTOP_DIR="parrot_all"
+username=$(whoami)
+#echo "$username"
+#check if user argument is provided. To denote total number of arguments passed we use $#
+#if [ $# -ne 1 ]; then
+ #   echo "Usuage: $0 <root_password>"
+  #  exit 1
+#fi
+user_groups=$(groups "$username")
+
+if [[ "$user_groups" != *sudo* ]]; then
+    echo "User $username not in sudo group. Adding....."
+    echo "After this operation you can remove user from sudo group if you wish. COMMAND: sudo gpasswd -d $username sudo"
+    echo "$ROOT_PASSWORD" | $(sudo usermod -a -G sudo $username)
+fi
+
+echo "Starting to Update and Upgrade your OS"
+
+echo "$ROOT_PASSWORD" | sudo -S apt-get update -y
+echo $(sudo -S apt-get upgrade -y)
+#echo "$ROOT_PASSWORD" | sudo -S apt-get upgrade -y
+#echo $(sudo apt-get update -y && sudo apt-get upgrade -y)
+
+#-----------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------
+sudo apt-get install mlocate -y
+sudo updatedb #to update mlocate database
+sudo apt install git -y
+
+cd ~/Desktop
+if [ ! -d ~/Desktop/$DESKTOP_DIR ]; then
+    mkdir $DESKTOP_DIR
+    cd $DESKTOP_DIR
+fi
+
+#-----------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------
+if [ ! -d /opt/discover ]; then
+    cd /opt
+    echo "Downloading discover for testing recon, vuln assessment etc. Run ./update.sh for updating wordlists and files in /opt"
+    echo $(git clone https://github.com/leebaird/discover.git) 
+else
+    echo "Discover already exists."
+fi
+
+cd /opt/discover
+chmod +x discover.sh
+chmod +x update.sh
+sudo ./update.sh
+
+if [ ! -f /usr/bin/discover ]; then
+    echo $(sudo ln -s ${PWD}/discover.sh /usr/bin/discover)
+fi
+
+#To check if file exists if [ -f "$FILE" ] and to check if a directory exists if [ -d "$DIRECTORY_NAME_FULL" ]
+#FILE=/etc/resolv.conf
+#if [ -f "$FILE" ]; then
+#    echo "$FILE exists."
+#fi
+DIRECTORY_NAME=/opt/SecLists
+if [ -d "$DIRECTORY_NAME" ]; then
+    echo "SecLists Already Exists."
+else
+    cd ~/Desktop/$DESKTOP_DIR
+    echo "Downloading SecLists which contain wordlists for fuzzing, username, password etc"
+    echo $(git clone https://github.com/danielmiessler/SecLists.git)
+fi
+
+#-----------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------
+cd ~/Desktop/$DESKTOP_DIR
+echo "Downloading TBomb. SMS and Call Bomber"
+echo $(git clone https://github.com/TheSpeedX/TBomb.git)
+
+#-----------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------
+cd ~/Desktop/$DESKTOP_DIR
+echo "Downloading Lynis. Linux System Audit "
+echo $(git clone https://github.com/CISOfy/lynis.git)
+
+#-----------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------
+cd ~/Desktop/$DESKTOP_DIR
+echo "Downloading Golismero. Vulnerability Assessment Scanner like Nikto"
+echo $(git clone https://github.com/golismero/golismero.git)
+echo $(sudo apt-get install python2.7 python2.7-dev python-pip python-docutils git perl nmap sslscan -y)
+cd golismero
+echo $(pip install -r requirements.txt)
+echo $(pip install -r requirements_unix.txt)
+echo $(sudo ln -s ${PWD}/golismero.py /usr/bin/golismero)
+
+#-----------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------
+cd ~/Desktop/$DESKTOP_DIR
+echo "Downloading Sublister. Subdomain Finder "
+echo $(git clone https://github.com/aboul3la/Sublist3r.git)
+cd Sublist3r
+sudo pip install -r requirements.txt
+
+#-----------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------
+cd ~/Desktop/$DESKTOP_DIR
+echo "Downloading TheFatRat .. Trojan Creator"
+echo $(git clone https://github.com/screetsec/TheFatRat.git)
+cd TheFatRat
+chmod +x setup.sh && sudo ./setup.sh 
+echo "If some modules are specified Not OK in install, install them manually with apt install"
+
+exit
+#-----------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------
+
+#-----------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------
+
